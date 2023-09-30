8856d639dbb4f0ac2fee34182f8d9a37418469fc
Michael Graf <mgraf@hotmail.ch>
2023-10-01 00:12:55 +0200

    Added dlerror function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 use std::error::Error;
 use std::ffi::CString;
-use std::os::raw::{c_void, c_char, c_int};
+use std::os::raw::{c_char, c_int, c_void};
 
 // #[link(name = "libc")]
-extern "system"
-{
+extern "system" {
     #[link_name = "dlopen"]
     fn _dlopen(filename: *const c_char, flags: c_int) -> *mut c_void;
     #[link_name = "dlclose"]
@@ -19,24 +18,20 @@ extern "system"
 /// handle returned by dlopen outside of this library.
 /// Every FileHandle corresponds to exactly one dlopen/dlclose calling pair.
 /// As such, there is no implementation of the Copy-Trait
-pub struct FileHandle
-{
+pub struct FileHandle {
     _private: *mut c_void,
 }
 
-impl FileHandle
-{
+impl FileHandle {
     /// Creates a handle that does not represent a file opened with dlopen.
     /// Use case: replacing a FileHandle field in a struct.
-    pub fn invalid() -> Self
-    {
+    pub fn invalid() -> Self {
         FileHandle {
-            _private: std::ptr::null_mut()
+            _private: std::ptr::null_mut(),
         }
     }
 
-    pub fn is_valid(&self) -> bool
-    {
+    pub fn is_valid(&self) -> bool {
         !self._private.is_null()
     }
 }
@@ -51,65 +46,65 @@ pub const RTLD_GLOBAL: i32 = 0x00100;
 pub const RTLD_LOCAL: i32 = 0;
 pub const RTLD_NODELETE: i32 = 0x01000;
 
-pub fn dlopen(filename: &str, flags: i32) -> Result<FileHandle, Box<dyn Error>>
-{
-    if filename == ""
-    {
-        let handle = unsafe
-        {
-            FileHandle{ _private: _dlopen(std::ptr::null(), flags) }
+pub fn dlopen(filename: &str, flags: i32) -> Result<FileHandle, Box<dyn Error>> {
+    if filename == "" {
+        let handle = unsafe {
+            FileHandle {
+                _private: _dlopen(std::ptr::null(), flags),
+            }
         };
         return Ok(handle);
-    }
-    else
-    {
+    } else {
         let cstr_filename;
-        match CString::new(filename)
-        {
-            Err(e) =>
-            {
-                return Err(Box::new(e))
-            },
-            Ok(value) =>
-            {
+        match CString::new(filename) {
+            Err(e) => return Err(Box::new(e)),
+            Ok(value) => {
                 cstr_filename = value;
             }
         }
 
-        let handle = unsafe
-        {
-            FileHandle{ _private: _dlopen(cstr_filename.as_ptr(), flags) }
+        let handle = unsafe {
+            FileHandle {
+                _private: _dlopen(cstr_filename.as_ptr(), flags),
+            }
         };
         return Ok(handle);
     }
 }
 
-pub fn dlclose(handle: FileHandle) -> i32
-{
-    unsafe
-    {
-        _dlclose(handle._private)
-    }
+pub fn dlclose(handle: FileHandle) -> i32 {
+    unsafe { _dlclose(handle._private) }
 }
 
-pub fn dlsym(handle: &FileHandle, symbol: &str) -> Result<*mut c_void, Box<dyn Error>>
-{
+pub fn dlsym(handle: &FileHandle, symbol: &str) -> Result<*mut c_void, Box<dyn Error>> {
     let cstr_symbol;
-    match CString::new(symbol)
-    {
-        Err(e) =>
-        {
+    match CString::new(symbol) {
+        Err(e) => {
             return Err(Box::new(e));
-        },
-        Ok(value) =>
-        {
+        }
+        Ok(value) => {
             cstr_symbol = value;
         }
     }
 
-    let symbol_handle = unsafe
-    {
-        _dlsym(handle._private, cstr_symbol.as_ptr())
-    };
+    let symbol_handle = unsafe { _dlsym(handle._private, cstr_symbol.as_ptr()) };
     Ok(symbol_handle)
+}
+
+pub fn dlerror() -> Result<Option<String>, std::str::Utf8Error> {
+    unsafe {
+        let message = _dlerror();
+        if message.is_null() {
+            return Ok(None);
+        }
+
+        let message = std::ffi::CStr::from_ptr(message);
+        match message.to_str() {
+            Err(err) => Err(err),
+            Ok(message) => {
+                let message = message.to_owned();
+                Ok(Some(message))
+            }
+        }
+    }
 }
